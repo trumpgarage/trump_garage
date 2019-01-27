c928ef52d8a60c6f431f5cd3e4f3ca74b2e660cd
alexbosmn <ab6816@ic.ac.uk>
2019-01-27 04:27:58 +0000

    aaaaaaaaaaaaaaaaaaaaaaaaa

--- interface.py
+++ interface.py
@@ -9,17 +9,21 @@ screen.geometry("500x1000")
 text = Text(screen, height = 50, width = 100)
 text.pack()
 
+
 def apress():
-    text.insert(END, chain.make_song())
+	text.insert(END,"\n\n\n")
+	text.insert(END,chain.make_song())
+
 
 def addtweet():
-	text.insert(END, chain.make_tweet())
+	text.insert(END,"\n\n\n")
+	text.insert(END,chain.make_tweet())
 
 
-btn = Button(screen, text = 'Generate song', width = 15, command = apress) 
+btn = Button(screen, text = 'Generate song', width = 20, command = apress) 
 btn.pack(side=LEFT)
 
-btn2 = Button(screen, text = 'Generate tweet', width = 15, command = addtweet)
+btn2 = Button(screen, text = 'Generate tweet', width = 20, command = addtweet)
 btn2.pack(side=RIGHT)
 
 mainloop()
